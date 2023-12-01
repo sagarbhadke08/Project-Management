f35307e82fb73e87f0f0d62b0fae7222f01ba68e
sagarbhadke08 <sagarbhadke08@gmail.com>
2023-12-01 07:58:12 +0530

    Project Manager App

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ProjectsSideBar from "./components/ProjectsSideBar";
+
+function App() {
+  return (
+    <main className="h-screen my-8">
+     
+     <ProjectsSideBar/>
+    </main  >
+  );
+}
+
+export default App;
---
+++ src/components/ProjectsSideBar.jsx
@@ -0,0 +1,13 @@
+
+
+
+export default function ProjectsSideBar() {
+    return <aside>
+
+       <h2>Basic Projects</h2> 
+       <div>
+        <button>+Add Projects</button>
+       </div>
+    <ul></ul>
+    </aside>
+}
