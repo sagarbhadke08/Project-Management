905c89642eefa9dc0fa4d51585ef8036a7649187
sagarbhadke08 <sagarbhadke08@gmail.com>
2023-12-07 23:29:04 +0530

    updated

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,4 @@
-export default function Button({children, ...props}) {
+export default function Button({ children, ...props }) {
     return <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-600
     text-stone-400 hover:bg-stone-700 hover:text-stone-100" {...props} >
         {children}
